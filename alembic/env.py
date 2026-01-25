7eec8592342814548e800f8a87e7801dce37cd04
Nat-aho <natascia.caria@gmail.com>
2026-01-25 16:43:06 +0100

    Alembic: Import SQLModel models for proper migration

--- alembic/env.py
+++ alembic/env.py
@@ -33,6 +33,9 @@ from sqlmodel.sql.sqltypes import AutoString
 from alembic import context
 from alguerisme.configs.database import DatabaseConfig
 from alguerisme.services.database import get_database_url
+from alguerisme.services.models import (
+    EntryURLs,  # noqa: F401 - Import to register with SQLModel
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
